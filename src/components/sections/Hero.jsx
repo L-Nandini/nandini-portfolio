e7fa113fb7619f64547016147e2e29bea7c8e5b4
L-Nandini <laknandu123@gmail.com>
2026-07-01 17:53:55 +0530

    changing educational details

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -85,7 +85,7 @@ function Hero() {
             </div>
 
             <div className="bg-gray-900 rounded-xl p-4 text-center shadow-lg hover:scale-105 transition duration-300">
-              <h3 className="text-3xl font-bold text-blue-400">9.3</h3>
+              <h3 className="text-3xl font-bold text-blue-400">8.9</h3>
               <p className="text-gray-400 text-sm mt-1">
                 CGPA
               </p>
